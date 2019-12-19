4227149d6dfa19c1c9f1c299964c39fd4a51d874
Lili-Mae Edwards <lilimae@hotmail.co.uk>
2019-12-19 14:07:57 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,11 +15,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {name: "Steven", age:"30", attitude: "confused"},
-        {name: "Nurse", age:"18", attitude: "hot-headed"}
+        {name: "Nurse", age:"44", attitude: "worried"}
       ]
    }
 }
-
-  
-
 end
